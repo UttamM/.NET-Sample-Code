ac086573eae7b9d8689bbfc4661d7f400695347d
Uttam Mogilicherla <umogilicherla@gmail.com>
2023-10-18 01:30:10 +0500

    Added sample code

---
+++ EventListingPageController.cs
@@ -0,0 +1,117 @@
+﻿using System.Web.Mvc;
+using CompanyName.Models.Pages;
+using EPiServer.Find;
+using EPiServer.Find.UI;
+using packagename.Framework.CMS.Web.Controllers;
+using PagedList;
+using log4net;
+using CompanyName.ViewModels;
+using EPiServer.Globalization;
+using System.Collections.Generic;
+using EPiServer.Find.Cms;
+using System;
+using CompanyName.Business.ExtensionMethods;
+using EPiServer.Core;
+using Castle.Core.Internal;
+
+namespace CompanyName.Controllers
+{
+    /// <summary>
+    /// Custom controller for event listing page.
+    /// </summary>
+    /// <seealso cref="packagename.Framework.CMS.Web.Controllers.PageControllerBase{CompanyName.Models.Pages.EventListingPage}" />
+    public class EventListingPageController : PageControllerBase<EventListingPage>
+    {
+        private const int MaxResults = 1000;
+        private readonly IClient _searchClient;
+        private readonly IFindUIConfiguration _findUIConfiguration;
+        private static readonly ILog logger = LogManager.GetLogger(typeof(EventListingPageController));
+
+        public EventListingPageController(
+            IClient searchClient,
+            IFindUIConfiguration findUIConfiguration)
+        {
+            _searchClient = searchClient;
+            _findUIConfiguration = findUIConfiguration;
+        }
+
+        [ValidateInput(false)]
+        public ActionResult Index(EventListingPage currentPage, IEnumerable<int> Region, IEnumerable<int> Industry, int? page)
+        {
+            Region = Region ?? new List<int>();
+            Industry = Industry ?? new List<int>();
+
+            var model = new EventListingViewModel(currentPage)
+            {
+                Regions = Region,
+                Industries = Industry,
+                PublicProxyPath = _findUIConfiguration.AbsolutePublicProxyPath(),
+            };
+
+            int pageIndex = page ?? 1;
+            int pageSize = currentPage.PageSize;
+
+            try
+            {
+                var query = _searchClient.Search<EventsDetailPage>();
+                if (!ContentReference.IsNullOrEmpty(currentPage.EventContainer))
+                {
+                    query = query.Filter(x => x.Ancestors().Match(currentPage.EventContainer.ToString()));
+                }
+
+                var results = query
+                                .FilterForVisitor()
+                                .Filter(p => p.Language.Name.Match(ContentLanguage.PreferredCulture.Name))
+                                .Filter(BuildRegionIndustryFilter(model))
+                                //.Filter(BuildDateFilter())
+                                .ExcludeDeleted()
+                                .CurrentlyPublished()
+                                .ApplyBestBets()
+                                .Skip((pageIndex - 1) * pageSize)
+                                .Take(pageSize)
+                                .OrderBy(x => x.StartDate)
+                                .GetContentResult();
+
+                model.Hits = new StaticPagedList<EventsDetailPage>(results, pageIndex, pageSize, results.TotalMatching);
+            }
+            catch (ServiceException ex)
+            {
+                logger.Error("Episerver Find threw an exception: " + ex.Message, ex);
+                model.FindError = true;
+            }
+
+            return View(model);
+        }
+
+        private FilterBuilder<EventsDetailPage> BuildRegionIndustryFilter(EventListingViewModel model)
+        {
+            var categoryFilter = _searchClient.BuildFilter<EventsDetailPage>();
+            if (model == null || (model.Regions.IsNullOrEmpty() && model.Industries.IsNullOrEmpty()))
+            {
+                // Select all filter
+                return categoryFilter.Or(x => x.Category.In(model.Categories.ToCategoryList()));
+            }
+
+            if (model.Regions.HasValue() && model.Industries.HasValue())
+            {
+                return categoryFilter
+                            .And(x => x.Category.In(model.Regions))
+                            .And(x => x.Category.In(model.Industries));
+            }
+
+            if (model.Regions.HasValue())
+            {
+                return categoryFilter.Or(x => x.Category.In(model.Regions));
+            }
+
+            return categoryFilter.Or(x => x.Category.In(model.Industries));
+        }
+
+        public FilterBuilder<EventsDetailPage> BuildDateFilter()
+        {
+            var dateFilter = _searchClient.BuildFilter<EventsDetailPage>();
+            dateFilter = dateFilter.And((x => x.StartDate.After(DateTime.Today.Date))).Or(x => x.StartDate.Match(DateTime.Today));
+            return dateFilter;
+        }
+    }
+}
---
+++ SearchPageController.cs
@@ -0,0 +1,100 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using CompanyName.Models.Pages;
+using CompanyName.Models.Pages.BaseData;
+using CompanyName.ViewModels;
+using EPiServer.Find;
+using EPiServer.Find.Framework.Statistics;
+using EPiServer.Find.Statistics;
+using EPiServer.Find.UI;
+using EPiServer.Find.UnifiedSearch;
+using EPiServer.Globalization;
+using log4net;
+using packagename.Framework.CMS.Web.Controllers;
+using PagedList;
+
+namespace CompanyName.Controllers
+{
+    /// <summary>
+    ///     Full implementation after finish story 9431 Search result page.
+    ///     see more @ https://packagename.visualstudio.com/CompanyName/_workitems/edit/9431
+    /// </summary>
+    /// <seealso cref="EPiServer.Web.Mvc.PageController{CompanyName.Models.Pages.SearchPage}" />
+    public class SearchPageController : PageControllerBase<SearchPage>
+    {
+        private const int MaxResults = 1000;
+        private readonly IClient _searchClient;
+        private readonly IFindUIConfiguration _findUIConfiguration;
+        private static readonly ILog logger = LogManager.GetLogger(typeof(SearchPageController));
+
+        public SearchPageController(
+            IClient searchClient,
+            IFindUIConfiguration findUIConfiguration)
+        {
+            _searchClient = searchClient;
+            _findUIConfiguration = findUIConfiguration;
+        }
+
+        [ValidateInput(false)]
+        public ActionResult Index(SearchPage currentPage, string q, int? page)
+        {
+            var model = new SearchContentViewModel(currentPage)
+            {
+                Query = (q ?? string.Empty).Trim(),
+                PublicProxyPath = _findUIConfiguration.AbsolutePublicProxyPath(),
+            };
+
+            int pageIndex = page ?? 1;
+            int pageSize = currentPage.PageSize;
+
+            ITypeSearch<ISearchContent> query = null;
+            UnifiedSearchResults results = null;
+
+            if (!string.IsNullOrWhiteSpace(model.Query))
+            {
+                query =
+                    _searchClient.UnifiedSearchFor(model.Query)
+                    .UsingSynonyms()
+                                //Include a facet whose value we can use to show the total number of hits
+                                //regardless of section. The filter here is irrelevant but should match *everything*.
+                                .TermsFacetFor(x => x.SearchSection)
+                                // Don't include pages in the results if the do not include in search box is checked
+                                .Filter(x => x.MatchTypeHierarchy(typeof(CompanyNameBasePageData)) & !((CompanyNameBasePageData)x).MetaData.NoIndex.Match(true))
+                                // Only show results from the current language branch
+                                .Filter(x => x.MatchTypeHierarchy(typeof(CompanyNameBasePageData)) & ((CompanyNameBasePageData)x).Language.Name.Match(ContentLanguage.PreferredCulture.Name))
+                                .FilterFacet("AllSections", x => x.SearchSection.Exists())
+                                //Fetch the specific paging page.
+                                .Skip((pageIndex - 1) * pageSize)
+                                .Take(pageSize)
+                                .ApplyBestBets();
+
+                var doNotTrackHeader = System.Web.HttpContext.Current.Request.Headers.Get("DNT");
+                // Should Not track when value equals 1
+                if (doNotTrackHeader == null || doNotTrackHeader.Equals("0"))
+                {
+                    query = query.Track();
+                }
+
+                var hitSpec = new HitSpecification
+                {
+                    HighlightTitle = model.CurrentPage.HighlightTitles,
+                    HighlightExcerpt = model.CurrentPage.HighlightExcerpts
+                };
+
+                try
+                {
+                    results = query.GetResult(hitSpec);
+                    model.Hits = new StaticPagedList<UnifiedSearchHit>(results, pageIndex, pageSize, results.TotalMatching);
+                    model.RelatedQueries = _searchClient.Statistics().GetDidYouMean(model.Query).Hits?.ToList().Select(x => x.Suggestion);
+                }
+                catch (ServiceException ex)
+                {
+                    logger.Error("Episerver Find threw an exception: " + ex.Message, ex);
+                    model.FindError = true;
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
